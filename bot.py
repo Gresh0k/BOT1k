45ff5c351a9ae9a8a1faaad0ab815aba10206ad5
Gresh0k <vinipyx2002@gmail.com>
2025-01-31 12:56:59 +0400

    Add files via upload

--- bot.py
+++ bot.py
@@ -1,82 +1,83 @@
 from aiogram import Bot, Dispatcher, types
-from aiogram.enums import ParseMode
-from aiogram.filters import Command
-from aiogram.types import InlineKeyboardMarkup, InlineKeyboardButton
-from aiogram.utils.keyboard import InlineKeyboardBuilder
-from aiogram.webhook.aiohttp_server import SimpleRequestHandler, setup_application
-from aiohttp import web
-import sqlite3
-import atexit
+from aiogram.contrib.middlewares.logging import LoggingMiddleware
+from aiogram.utils import executor
+from aiogram.types import ReplyKeyboardRemove, ReplyKeyboardMarkup, KeyboardButton, InlineKeyboardMarkup, InlineKeyboardButton
+import aiosqlite
+import asyncio
 
 API_TOKEN = '7801123648:AAHX0_w3EF3_2Z4XlXjH5BFfjD0L4EBH2aA'
 
-# Инициализация бота и диспетчера
-bot = Bot(token=API_TOKEN,)
-dp = Dispatcher()
+bot = Bot(token=API_TOKEN)
+dp = Dispatcher(bot)
 
-# Подключение к базе данных
-conn = sqlite3.connect('bot_database.db', check_same_thread=False)
-cursor = conn.cursor()
 
-# Создание таблицы пользователей
-cursor.execute('''
-    CREATE TABLE IF NOT EXISTS users (
-        id INTEGER PRIMARY KEY,
-        user_id INTEGER NOT NULL UNIQUE,
-        username TEXT
-    )
-''')
-conn.commit()
 
-# Создание клавиатуры
-builder = InlineKeyboardBuilder()
-builder.add(InlineKeyboardButton(text="Список серверов", callback_data="button1"))
+async def NewTable():
+    # Создание подключения к базе данных (асинхронно)
+    async with aiosqlite.connect('bot_database.db') as db:
+        # Создание таблицы пользователей
+        await db.execute('''
+            CREATE TABLE IF NOT EXISTS users (
+                id INTEGER PRIMARY KEY,
+                user_id INTEGER NOT NULL UNIQUE,
+                username TEXT
+            )
+        ''')
+        # Подтверждение изменений
+        await db.commit()
 
-@dp.message(Command("start"))
+ 
+
+
+keyboard = InlineKeyboardMarkup(row_width=1)
+button = InlineKeyboardButton(text="Список серверов", callback_data="button1")
+keyboard.add(button)
+
+@dp.message_handler(commands=['start'])
 async def send_welcome(message: types.Message):
-    await message.answer(
+    await bot.send_message(
+        message.chat.id,
         """Привет! Я бот для удалённого управления сервером Minecraft!
-        Через меня, ты можешь запускать и останавливать свой сервер в Minecraft, а также вводить команды в консоль команды и смотреть онлайн сервера!
-        Что бы продолжить, выбери один из предложенных вариантов кнопок""",
-        reply_markup=builder.as_markup()
-    )
-    
+Через меня, ты можешь запускать и останавливать свой сервер в Minecraft,  а также вводить команды в консоль команды и смотреть онлайн сервера!
+Что бы продолжить, выбери один из предложенных вариантов кнопок""",
+        reply_markup=keyboard)
+
+    #Создание таблица базы данных, если её нет
+    await NewTable()    
+
+    #Перенос ID и UserName в переменные
     user_id = message.from_user.id
     username = message.from_user.username
 
-    # Сохранение данных пользователя в базу
-    cursor.execute('''
-        INSERT OR IGNORE INTO users (user_id, username) VALUES (?, ?)
-    ''', (user_id, username))
-    conn.commit()
+    #Вызов базы данных
+    async with aiosqlite.connect('bot_database.db') as db:
+        # Сохранение данных пользователя в базу
+        await db.execute('''
+            INSERT OR IGNORE INTO users (user_id, username) VALUES (?, ?)
+        ''', (user_id, username))
+        # Подтверждение изменений
+        await db.commit()
 
-# Создание второй клавиатуры
-builder2 = InlineKeyboardBuilder()
-builder2.add(InlineKeyboardButton(text='Голодные игры', callback_data="btn1"))
+keyboard2 = InlineKeyboardMarkup(row_width=1)
+button2 = InlineKeyboardButton(text='Голодные игры', callback_data="btn1")
+keyboard2.add(button2)
 
-@dp.callback_query(lambda c: c.data == 'button1')
+@dp.callback_query_handler(lambda c: c.data == 'button1')
 async def process_callback_button(callback_query: types.CallbackQuery):
-    await callback_query.answer()
+    # Отвечаем на callback (обязательно)
+    await bot.answer_callback_query(callback_query.id)
     
-    await callback_query.message.edit_text(
-        "Выберите один из уже существующих серверов или создайте новый",
-        reply_markup=builder2.as_markup()
+    # Изменяем сообщение
+    await bot.edit_message_text(
+        chat_id=callback_query.message.chat.id,
+        message_id=callback_query.message.message_id,
+        text="Выберите один из уже существующих серверов или создайте новый",
+        reply_markup=keyboard2
     )
 
-# Закрытие соединения с базой при завершении работы
-@atexit.register
-def close_db():
-    conn.close()
 
-async def on_startup(bot: Bot):
-    # Логика, которая выполняется при запуске бота
-    pass
-
-async def on_shutdown(bot: Bot):
-    # Логика, которая выполняется при завершении работы бота
-    pass
-
+async def main():
+    await dp.start_polling()
 if __name__ == '__main__':
-    dp.startup.register(on_startup)
-    dp.shutdown.register(on_shutdown)
-    dp.run_polling(bot)
+    executor.start_polling(dp, skip_updates=True)
+
